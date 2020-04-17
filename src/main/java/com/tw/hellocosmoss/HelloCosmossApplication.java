815ae479e6a0ca2a92fb3f08bc48fe9c3892919f
abhisekharh1988 <abhisek.harh@gmail.com>
2020-04-17 10:27:51 +0530

    First Commit

---
+++ src/main/java/com/tw/hellocosmoss/HelloCosmossApplication.java
@@ -0,0 +1,13 @@
+package com.tw.hellocosmoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCosmossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCosmossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/hellocosmoss/HelloCosmossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.hellocosmoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCosmossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
